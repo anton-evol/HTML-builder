65f7aec8f807905cf52283e15e951ffb820c9cc5
Anton Novikov <3483778@gmail.com>
2022-05-21 18:44:11 +0300

    feat: add 04 task solution (Sat, May 21, 2022 06:44:11 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+const { copyFile } = require('fs/promises');
+const path = require('path');
+const dirPathSource = path.resolve(__dirname, 'files');
+const dirPathDestination = path.resolve(__dirname, 'files-copy');
 
+fs.promises.rm(dirPathDestination, { recursive: true, force: true }).then(function () {
+  fs.promises.mkdir(dirPathDestination, { recursive: true }).then(function () {
+    readdir(dirPathSource).then((filenames) => {
+      for (let filename of filenames) {        
+        let sourceFullName = path.resolve(dirPathSource, filename);
+        let destinationFullName = path.resolve(dirPathDestination, filename);
+        copyFile(sourceFullName, destinationFullName);
+      }
+    });
+  });
+  console.log('Копирование/сихронизация успешно завершены!');
+});
